9d73ef1d21fb3e2c90158e5d7a58b3030af34d43
Safnas <iamsafnas@gmail.com>
2020-03-22 14:03:36 +0530

    Update navbar.component.js

--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-expand-lg">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <Link to="/" className="navbar-brand">ExerTracker</Link>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                  <span class="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse">
+                <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item active">
                             <Link to="/" className="nav-link">Exercises</Link>
