e5473c70ec644df6f1010badf0fc3ec63bc75cb9
Safnas <iamsafnas@gmail.com>
2020-03-22 14:25:01 +0530

    Update navbar.component.js

--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -4,25 +4,18 @@ import { Link } from 'react-router-dom';
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <Link to="/" className="navbar-brand">ExerTracker</Link>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                 <span class="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item active">
-                            <Link to="/" className="nav-link">Exercises</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/create" className="nav-link">Create Exercise Log</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/user" className="nav-link">Create User</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+            
+           <Navbar bg="dark" expand="lg">
+  <Navbar.Brand href="/">ExerTracker</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="/">Exercises</Nav.Link>
+      <Nav.Link href="/create">Create Exercise Log</Nav.Link>
+      <Nav.Link href="/user">Create User</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
         );
     }
 }
