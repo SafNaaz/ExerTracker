7e547d5e7074e71ab5cedf522f06309a2136bf58
Safnas <iamsafnas@gmail.com>
2020-03-22 13:46:09 +0530

    Update navbar.component.js

--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -6,15 +6,18 @@ export default class Navbar extends Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-expand-lg">
                 <Link to="/" className="navbar-brand">ExerTracker</Link>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                 <span class="navbar-toggler-icon"></span>
+                </button>
                 <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="navbar-item">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
                             <Link to="/" className="nav-link">Exercises</Link>
                         </li>
-                        <li className="navbar-item">
+                        <li className="nav-item">
                             <Link to="/create" className="nav-link">Create Exercise Log</Link>
                         </li>
-                        <li className="navbar-item">
+                        <li className="nav-item">
                             <Link to="/user" className="nav-link">Create User</Link>
                         </li>
                     </ul>
