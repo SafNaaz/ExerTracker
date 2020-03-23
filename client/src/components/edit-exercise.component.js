733402b5896250a97f8eddd9509bacd14b8d0ab5
Safnas <iamsafnas@gmail.com>
2020-03-23 17:14:26 +0530

    Update edit-exercise.component.js

--- client/src/components/edit-exercise.component.js
+++ client/src/components/edit-exercise.component.js
@@ -71,7 +71,7 @@ export default class EditExercise extends Component {
 
         console.log(exercise);
 
-        axios.post('http://localhost:5000/exercises/update/' + this.props.match.params.id, exercise)
+        axios.post('/exercises/update/' + this.props.match.params.id, exercise)
             .then(res => console.log(res.data));
 
         window.location = '/';
