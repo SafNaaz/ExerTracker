9a58fe52f10207d80cf9be6c2721530f259c9701
Safnas <iamsafnas@gmail.com>
2020-03-23 17:12:56 +0530

    Update create-exercise.component.js

--- client/src/components/create-exercise.component.js
+++ client/src/components/create-exercise.component.js
@@ -28,7 +28,7 @@ export default class CreateExercise extends Component {
     }
 
     componentDidMount() {
-        axios.get("http://localhost:5000/users")
+        axios.get("/users")
             .then(res => {
                 if (res.data.length > 0) {
                     this.setState({
@@ -75,7 +75,7 @@ export default class CreateExercise extends Component {
 
         console.log(exercise);
 
-        axios.post('http://localhost:5000/exercises/add', exercise)
+        axios.post('/exercises/add', exercise)
             .then(res => console.log(res.data));
 
         window.location = '/';
