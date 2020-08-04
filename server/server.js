a238aae57ecd422595e52a2a7653206d776cc0a5
Safnas <iamsafnas@gmail.com>
2020-08-04 17:32:28 +0530

    server running on docker connected to mongo

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const path = require("path");
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
